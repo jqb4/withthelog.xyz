1f970d4d91dd7e8ebab7b058858f4e797cd0e754
Johan <134583362+withthelog@users.noreply.github.com>
2024-10-13 11:24:54 +0200

    Update lanyard.js

--- lanyard.js
+++ lanyard.js
@@ -1,1 +1,19 @@
+const ws = new WebSocket("wss://api.lanyard.rest/socket");
 
+ws.onopen = () => {
+  ws.send(
+    JSON.stringify({
+      op: 2,
+      d: {
+        subscribe_to_id: "712648730423197697",
+      },
+    })
+  );
+};
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  if (data.t === "INIT_STATE" || data.t === "PRESENCE_UPDATE") {
+    console.log(data.d);
+  }
+};
